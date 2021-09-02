d89ef59ebad525184d1b79191d72dc66d765a1df
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-02 16:04:08 +0200

    filter select

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ const root = new Vue({
     el: "#root",
     data: {
         compilation: [],
+        options: [],
+        selectedFilter: 'all'
+
     },
-    methods: {},
+    methods: {
+
+    },
 
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
@@ -11,14 +16,35 @@ const root = new Vue({
                 let compilation = res.data.response;
                 this.compilation = compilation;
             })
+
     },
 
     computed: {
-        sortedCompilation() {
+        filteredCompilation() {
+            // sort by year
             const sorted = this.compilation.sort((a, b) => {
                 return a.year - b.year;
+            });
+
+            // filter by genre
+            const filtered = sorted.filter((song) => {
+
+                return song.genre === this.selectedFilter || this.selectedFilter == 'all';
             })
-            return sorted;
+
+            return filtered;
+        },
+
+        // generate options 
+        renderOptions() {
+            this.compilation.forEach(song => {
+                if (!this.options.includes(song.genre)) {
+                    this.options.push(song.genre);
+                };
+            });
+
+            return this.options;
+
         }
 
     },
