aa299a2574397b8399fed927420baada9ee572a4
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-02 14:33:09 +0200

    axios import + vue v-for card

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const root = new Vue({
     el: "#root",
-    data: {},
-    methods: {}
+    data: {
+        compilation: null,
+    },
+    methods: {},
+    created() {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((res) => {
+                const compilation = res.data.response;
+                console.log(compilation);
+                this.compilation = compilation;
+            })
+    }
+
+
 
 
 
